fd46618d95970807c23fcd2771ccc241b81010cb
marshalltaylor <silvercraft@gmail.com>
2015-04-13 23:41:10 -0600

    Writing docs

--- blink_fsm/blink_fsm.ino
+++ blink_fsm/blink_fsm.ino
@@ -1,36 +1,31 @@
+//Define the states of the machine
 #define LED_OFF 0
 #define LED_ON 1
 
+//Here is the state variable
 uint8_t fsm_state = LED_OFF;
 
-// the setup function runs once when you press reset or power the board
+
 void setup() {
-  // initialize digital pin 13 as an output.
-  pinMode(13, OUTPUT);
+  pinMode(13, OUTPUT);  // initialize digital pin 13 as an output.
 }
 
-// the loop function runs over and over again forever
-void loop()
-{
-  //state machine
-  uint8_t next_fsm_state = fsm_state;  //Next state defaults to current state
 
+void loop(){
+  //state machine
   switch (fsm_state)
   {
-    case LED_OFF:
-    //Statements to execute every time LED_OFF is reached
+    case LED_OFF:  //Statements to execute every time LED_OFF is reached
       digitalWrite(13, LOW);
-      next_fsm_state = LED_ON;
+      fsm_state = LED_ON;
       break;
-    case LED_ON:
-      //Statements to execute every time LED_ON is reached
+    case LED_ON:   //Statements to execute every time LED_ON is reached
       digitalWrite(13, HIGH);
-      next_fsm_state = LED_OFF;
+      fsm_state = LED_OFF;
       break;
     default:
       break;
   }
-  fsm_state = next_fsm_state;
 
   delay(1000);              // wait for a second
 }
